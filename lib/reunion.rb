d0607b5959f24772ba73696ae5df6c2518115704
maybemushrooms <thecraftedgem@tuta.io>
2018-07-29 13:28:54 -0600

    iteration 3 done

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,6 +1,15 @@
 require './lib/activity'
 
 class Reunion
+    attr_reader :activities,
+                :location
+    
     def initialize(location, activity)
+        @activities = []
+        @location = location
+    end
+
+    def add_activity(activity)
+     @activities << activity
     end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,7 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require 'pry'
 
 class ReunionTest < Minitest::Test
  
@@ -11,5 +12,21 @@ class ReunionTest < Minitest::Test
      activity.add_participants("Lisa", 50.0)
      activity.add_participants("Greg", 40.0)
      assert_instance_of Reunion, reunion
+   end
+
+   def test_it_has_location
+    reunion = Reunion.new("Rec Center", "Golf")
+    assert_equal "Rec Center", reunion.location
+   end
+
+   def test_it_can_add_activities
+    reunion = Reunion.new("Rec Center", "Golf")
+    activity = Activity.new("Golf", 20.0)
+    activity2 = Activity.new("Paintball", 200.0)
+    assert_equal [], reunion.activities
+    reunion.add_activity(activity)
+    reunion.add_activity(activity2)
+    expected = [activity, activity2]
+    assert_equal expected, reunion.activities
    end
 end
